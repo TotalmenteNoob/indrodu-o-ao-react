f5a133bc3980056bc4b85059e14f6a8808b45088
Victor Gabriel <104654809+TotalmenteNoob@users.noreply.github.com>
2023-04-12 00:51:26 -0300

    sexto commit

--- services/apiFilmes.js
+++ services/apiFilmes.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiFilmes = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     headers: {
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMWE2NjdhOTRiMGU1Mjk1ZTlkOTc4ODQ2NGE2Nzc3YSIsInN1YiI6IjY0MzVlMTQ3OTQ2MzE4MDBiZGZmMWVmOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7uqrm0XsKu4ef4P5HTMg9FuC7qjMhRP5gsHYHkZuh8M'
+        Authorization: ''
     }
 })
 
